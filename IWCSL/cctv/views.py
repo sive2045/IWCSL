fc30ce01b2f8e39f3018d0784183ea3ad33686c6
sive2045 <lc9902130509@gmail.com>
2021-09-02 22:44:40 +0900

    to shortcut

--- IWCSL/cctv/views.py
+++ IWCSL/cctv/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Event
 
 def index(request):
-    return HttpResponse("Hello world! This is cctv index page!")
+    latest_event_list = Event.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_event_list' : latest_event_list,
+    }
+    return render(request, 'cctv/index.html', context)
+
+def detail(request, event_id):
+    return HttpResponse("You're looking at question %s" %event_id)
