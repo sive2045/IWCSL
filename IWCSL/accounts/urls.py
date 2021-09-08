341c3989dd032aee29725872fa01623349219862
sive2045 <lc9902130509@gmail.com>
2021-09-08 23:04:25 +0900

    add login form

---
+++ IWCSL/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('', views.login, name='login'),
+    path('home/', views.home, name='home'),
+    path('logout/', views.logout, name='logout'),
+]  
