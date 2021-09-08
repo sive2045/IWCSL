888bd4713994deefbd0d97973a18d4f763a60490
sive2045 <lc9902130509@gmail.com>
2021-09-08 23:04:41 +0900

    update

--- IWCSL/cctv/urls.py
+++ IWCSL/cctv/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'cctv'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:event_id>/', views.detail, name='detail'),
